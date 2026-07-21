88d4c5b9eabb780ac714108e272dee64d12376be
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-07-21 22:51:38 +0600

    Expand rental cart listing data

--- app/src/main/java/com/example/foodmobochain/model/RentalCart.java
+++ app/src/main/java/com/example/foodmobochain/model/RentalCart.java
@@ -1,2 +1,17 @@
 package com.example.foodmobochain.model;
-public class RentalCart { public String id,name,location,description; public double dailyRate; public boolean available; public RentalCart(){} }
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+public class RentalCart {
+    public String id;
+    public String name;
+    public String location;
+    public String description;
+    public String imageUrl;
+    public double dailyRate;
+    public double deposit;
+    public boolean available;
+
+    public RentalCart() { }
+}
